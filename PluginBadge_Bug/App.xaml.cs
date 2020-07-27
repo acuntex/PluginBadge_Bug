20caf59bb42e5035fdd524c7378ca32d608d039f
Werner Welsch <werner@welsch.cc>
2020-07-27 11:53:01 +0200

    initial commit

---
+++ PluginBadge_Bug/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace PluginBadge_Bug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new TestTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
